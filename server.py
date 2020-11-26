ad88f8c006340df23a84d6a0d89bc52ff0ac6084
ManfredoFac <manfredo.facchini@yahoo.it>
2020-11-26 15:37:23 +0100

    ora il server genera le due chiavi in due file diversi

--- server.py
+++ server.py
@@ -1,6 +1,21 @@
 import socket as sk
 import sys
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+
 if __name__ == '__main__':
+
+    key = RSA.generate(2048)            #generazioni chiave privata per il server
+    private_key = key.export_key()
+    f = open('serverPrivKey.pem', 'wb')          #essendo always on si può fare all'inizio e pace
+    f.write(private_key)
+    f.close()
+
+    public_key = key.publickey().export_key()  #generazione chiave pubblica
+    f = open('servePubKey.pem', 'wb')
+    f.write(public_key)
+    f.close()
+
     host = sk.gethostname()
     port = 12345
     socket = sk.socket()

7f79d630469abdc086a40a6d729f923dd4cf807e
Akraier <72752917+Akraier@users.noreply.github.com>
2020-11-26 16:29:06 +0100

    prova

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket as sk
-
+from crypto.PublicKey import RSA
 
 if __name__ == '__main__':
     host = sk.gethostname()
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     socket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
     socket.connect((host, port))
     socket.sendall(bytes('Ciao bella!', 'utf-8'))
+    print('aooooo')
     ricevuto = socket.recv(1024)
     print(ricevuto)
     socket.close()
